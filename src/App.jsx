8ea64ecd4eb6a1b3cb2bdf3a114d49450e4c1bc7
tatianasnook <tatsnook@gmail.com>
2024-06-14 18:03:55 -0500

    React JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import * as React from 'react';
-import './App.css';
+// import './App.css';
 
-const title = 'React';
+const welcome = {
+  title: 'React',
+  greeting: 'Hey'
+};
+
+function getTitle(title){
+  return title;
+}
 
 function App() {
 
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>
+        {welcome.greeting} {welcome.title}
+      </h1>
+
+      <h2>Hello {getTitle('React')}</h2>
+
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='text'/>
+
     </div>
   );
 }
