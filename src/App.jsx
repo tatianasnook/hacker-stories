263ddfd3660dfc744e4d09ae5a8606d1fb51165c
tatianasnook <tatsnook@gmail.com>
2024-07-26 18:12:45 -0500

    added useRef hook

--- src/App.jsx
+++ src/App.jsx
@@ -49,10 +49,12 @@ const App = () => {
       <h1>My Hacker Stories</h1>
       <InputWithLabel
         id='search'
-        label="Search"
         value={searchTerm}
+        isFocused
         onInputChange={handleSearch}
-      />
+      >
+        <strong>Search: </strong>
+      </InputWithLabel>
       <hr />
       <List list={searchedStories} />
     </div>
--- src/components/InputWithLabel.jsx
+++ src/components/InputWithLabel.jsx
@@ -1,14 +1,25 @@
+import { useEffect, useRef } from "react";
 
 
-const InputWithLabel = ({ id, label, value, type="text", onInputChange}) => {
+const InputWithLabel = ({ id, value, type="text", onInputChange, isFocused, children}) => {
+  const inputRef = useRef();
+
+  useEffect(() => {
+    if(isFocused && inputRef.current){
+      inputRef.current.focus();
+    }
+  }, [isFocused])
+  
   return (
     <>
-      <label htmlFor="id">{label}</label>
+      <label htmlFor={id}>{children}</label>
       &nbsp;
       <input 
+        ref={inputRef}
         id={id}
         type={type}
         value={value}
+        autoFocus={isFocused}
         onChange={onInputChange}
       />
     </>
